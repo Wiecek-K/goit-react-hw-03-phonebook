54a9f8b6bd7e884bf8a4a049f9e5dc774f27fcb9
Wiecek-K <krystiandwiecek@gmail.com>
2023-06-09 04:17:05 +0200

    end task

--- src/components/Phonebook.jsx
+++ src/components/Phonebook.jsx
@@ -9,14 +9,29 @@ class Phonebook extends Component {
     super();
     this.state = {
       contacts: [
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+        // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
       ],
       filter: '',
     };
   }
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parseContacts = JSON.parse(contacts);
+    if (parseContacts) {
+      this.setState({ contacts: parseContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+
 
   handleSubmit = evt => {
     evt.preventDefault();
